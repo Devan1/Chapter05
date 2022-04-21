a2fe11f47da171795bc0b020120eb5f573c0f17f
Devan Fields <24418547+Devan1@users.noreply.github.com>
2022-04-21 18:25:06 -0500

    working

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Packt.Shared
 {
-    public class Person: Object
+    public class Person : Object
     {
         //fields
         public string Name;
@@ -12,5 +12,43 @@ namespace Packt.Shared
         public WondersOfTheAncientWorld FavoriteAncientWonder;
         public WondersOfTheAncientWorld BucketList;
         public List<Person> Children = new List<Person>();
+        public const string Species = "Homo Sapien";
+        public readonly string HomePlanet = "Earth";//should use read only instead of constants
+        public readonly DateTime Instantiated;
+
+        //constructors
+        public Person()
+        {
+            //set default values for fields, including read only fields
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        //methods
+        public void WriteToConsole()
+        {
+            Console.WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+        }
+
+        public string GetOrigin()
+        {
+            return $"{Name} was born on {HomePlanet}";
+        }
+
+        //tuples
+        public (string, int) GetFruit()
+        {
+            return ("Apples", 5);
+        }
+        public (string Name, int Number) GetNamedFruit()
+        {
+            return (Name: "Apples", Number: 5);
+        }
     }
 }
--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -66,3 +66,34 @@ gerrierAccount.Balance = 98M;
 WriteLine(format: "{0} earned {1:C} interest.",
     arg0: gerrierAccount.AccountName,
     arg1: gerrierAccount.Balance* BankAccount.InterestRate);
+
+WriteLine($"{bob.Name} is a {Person.Species}");
+WriteLine($"{bob.Name} is from the planet {bob.HomePlanet}");
+
+Person blankPerson = new();
+
+WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}",
+    arg0: blankPerson.Name,
+    arg1: blankPerson.HomePlanet,
+    arg2: blankPerson.Instantiated);
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+
+WriteLine(format: "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}",
+    arg0: gunny.Name,
+    arg1: gunny.HomePlanet,
+    arg2: gunny.Instantiated);
+
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"{fruitNamed.Name}, {fruitNamed.Number} there are. ");
+
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children. ");
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children. ");
