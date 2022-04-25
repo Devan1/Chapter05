8da3da09725d496db6c49ac28b79dcaab8cf19fa
Devan Fields <24418547+Devan1@users.noreply.github.com>
2022-04-25 18:06:46 -0500

    working

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -97,3 +97,74 @@ var thing1 = ("Neville", 4);
 WriteLine($"{thing1.Item1} has {thing1.Item2} children. ");
 var thing2 = (bob.Name, bob.Children.Count);
 WriteLine($"{thing2.Name} has {thing2.Count} children. ");
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+
+//doconstructing a person
+var (name1, dob1) = bob;
+WriteLine($"Deconstructed: {name1}, {dob1}");
+
+var (name2, dob2, fav2) = bob;
+WriteLine($"Deconstructed: {name2}, {dob2}, {fav2}");
+
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHello("Emily"));
+
+WriteLine(bob.OptionalParameters("Jump", 98.5, false));
+WriteLine(bob.OptionalParameters(number: 52.7, command: "Hide!"));
+WriteLine(bob.OptionalParameters(command: "Poke", active: false));
+WriteLine(bob.OptionalParameters(number: 34.4, active: true));
+
+int a = 10;
+int b = 20;
+int c = 30;
+
+WriteLine($"Before: a = {a}, b = {b}, c = {c}");
+bob.PassingParameters(a, ref b, out c);
+WriteLine($"After: a = {a}, b = {b}, c = {c}");
+
+Person sam = new()
+{
+    Name = "Sam",
+    DateOfBirth = new(1972, 1, 27)
+};
+WriteLine(sam.Origin);
+WriteLine(sam.Greeting);
+WriteLine(sam.Age);
+
+sam.FavoriteIceCream = "Chocolate";
+WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+sam.FavoritePrimaryColor = "red";
+WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}");
+
+sam.Children.Add(new() { Name = "Charlie" });
+sam.Children.Add(new() { Name = "ella" });
+
+WriteLine($"Sam's first child is {sam.Children[0].Name}");
+WriteLine($"Sam's second child is {sam.Children[1].Name}");
+WriteLine($"Sam's first child is {sam[0]}");
+WriteLine($"Sam's second child is {sam[1]}");
+
+object[] passengers =
+{
+    new FirstClassPassenger { AirMiles = 1_419},
+    new FirstClassPassenger { AirMiles = 16_562},
+    new BusinessClassPassenger(),
+    new CoachClassPassenger { CarryOnKG = 25.7},
+    new CoachClassPassenger { CarryOnKG = 0},
+};
+foreach(object passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35000   => 1500M,
+        FirstClassPassenger p when p.AirMiles > 15000   => 1750M,
+        FirstClassPassenger _                           => 2000M,
+        BusinessClassPassenger _                        => 1000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0   => 500M,
+        CoachClassPassenger _                           => 650M,
+        _                                               => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
